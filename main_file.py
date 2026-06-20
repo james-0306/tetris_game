5937d27591e0a37a21db21f18ed11e481832e0d5
james malitan <jamesmalitan21@gmail.com>
2026-06-20 11:41:50 +0800

    while double cheking I found a single variable already fix it

--- main_file.py
+++ main_file.py
@@ -16,8 +16,8 @@ next_rect = pygame.Rect(320, 215, 170, 180)
 screen = pygame.display.set_mode((500, 620))
 pygame.display.set_caption("Tetris")
 
-clock = pygame.time.Clock()
-game = Game()
+clock_game = pygame.time.Clock()
+game_data = Game()
 
 game_update_event = pygame.USEREVENT
 pygame.time.set_timer(game_update_event, 200)
@@ -28,36 +28,36 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if game.game_over:
-                game.reset_game()
+            if game_data.game_over:
+                game_data.reset_game()
             elif event.key == pygame.K_LEFT:
-                game.move_left()
+                game_data.move_left()
             elif event.key == pygame.K_RIGHT:
-                game.move_right()
+                game_data.move_right()
             elif event.key == pygame.K_DOWN:
-                game.move_down()
-                game.update_score(0, 1)
+                game_data.move_down()
+                game_data.update_score(0, 1)
             elif event.key == pygame.K_UP:
-                game.rotate()
-        if event.type == game_update_event and not game.game_over:
-            game.move_down()
+                game_data.rotate()
+        if event.type == game_update_event and not game_data.game_over:
+            game_data.move_down()
 
     screen.fill(Colors.dark_blue)
-    score_value_surface = title_font.render(str(game.score), True, Colors.white)
+    score_value_surface = title_font.render(str(game_data.score), True, Colors.white)
 
     screen.blit(score_surface, (365, 20))
     screen.blit(next_surface, (375, 180))
 
-    if game.game_over:
+    if game_data.game_over:
         screen.blit(game_over_surface, (320, 450))
 
     pygame.draw.rect(screen, Colors.light_blue, score_rect, 0, 10)
     screen.blit(score_value_surface, score_value_surface.get_rect(centerx=score_rect.centerx, centery=score_rect.centery))
     pygame.draw.rect(screen, Colors.light_blue, next_rect, 0, 10)
-    game.draw_game(screen)
+    game_data.draw_game(screen)
 
     pygame.display.update()
-    clock.tick(60)
+    clock_game.tick(60)
 
 
 
