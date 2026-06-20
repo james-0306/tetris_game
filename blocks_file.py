08ee82b40da04c0b05d170eb80baad9548f14512
james malitan <jamesmalitan21@gmail.com>
2026-06-20 09:17:00 +0800

    making the blocks file and the positioning to add to rows and columns

---
+++ blocks_file.py
@@ -0,0 +1,17 @@
+import pygame
+from color_file import Colors
+
+class Position:
+    def __init__(self, row, column):
+        self.row = row
+        self.column = column
+
+class Blocks:
+    def __init__(self, id):
+        self.id = id
+        self.cells = {}
+        self.cell_size = 30
+        self.row_offset = 0
+        self.column_offset = 0
+        self.rotation_state = 0
+        self.colors = Colors.get_cell_colors()
