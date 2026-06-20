fd133c4eeaf96ec6e7f868e0e2735185a9c96c1f
james malitan <jamesmalitan21@gmail.com>
2026-06-20 09:12:08 +0800

    finalizing the grid file by addinf different kind of function that will help the blocks to know their location and what the blocks should do

--- grid_file.py
+++ grid_file.py
@@ -20,3 +20,39 @@ class Grid:
             if self.grid[row][column] == 0:
                 return False
         return True
+
+    def clear_row(self, row):
+        for column in range(self.num_cols):
+            self.grid[row][column] = 0
+
+    def move_row_down(self, row, num_rows):
+        for column in range(self.num_cols):
+            self.grid[row + num_rows][column] = self.grid[row][column]
+            self.grid[row][column] = 0
+
+    def clear_full_rows(self):
+        completed = 0
+        for row in range(self.num_rows - 1, 0, -1):
+            if self.is_row_full(row):
+                self.clear_row(row)
+                completed += 1
+            elif completed > 0:
+                self.move_row_down(row, completed)
+        return completed
+
+    def reset_grid(self):
+        for row in range(self.num_rows):
+            for column in range(self.num_cols):
+                self.grid[row][column] = 0
+
+    def draw_grid(self, screen):
+        for row in range(self.num_rows):
+            for column in range(self.num_cols):
+                cell_value = self.grid[row][column]
+                cell_rect = pygame.Rect(
+                    column * self.cell_size + 11,
+                    row * self.cell_size + 11,
+                    self.cell_size - 1,
+                    self.cell_size - 1
+                )
+                pygame.draw.rect(screen, self.colors[cell_value], cell_rect)
