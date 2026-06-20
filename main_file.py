5a2b17b55541a6e510328b41d1ce4b753cc67362
james malitan <jamesmalitan21@gmail.com>
2026-06-20 11:00:12 +0800

    adding the visual for the game

--- main_file.py
+++ main_file.py
@@ -4,3 +4,8 @@ from color_file import Colors
 from game_file import Game
 
 pygame.init()
+
+title_font = pygame.font.Font(None, 40)
+score_surface = title_font.render("Score", True, Colors.white)
+next_surface = title_font.render("Next", True, Colors.white)
+game_over_surface = title_font.render("Game Over", True, Colors.white)
