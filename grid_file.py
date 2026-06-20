87ab54f9427c66b7700a41c9f18bdf4d840b6c7c
james malitan <jamesmalitan21@gmail.com>
2026-06-20 08:54:57 +0800

    making a function to know if the blocks is in the rows and column

--- grid_file.py
+++ grid_file.py
@@ -9,3 +9,14 @@ class Grid:
         self.grid = [[0 for _ in range(self.num_cols)] for _ in range(self.num_rows)]
         self.colors = Colors.get_cell_colors()
 
+    def is_inside(self, row, column):
+        return 0 <= row < self.num_rows and 0 <= column < self.num_cols
+
+    def is_empty(self, row, column):
+        return self.grid[row][column] == 0
+
+    def is_row_full(self, row):
+        for column in range(self.num_cols):
+            if self.grid[row][column] == 0:
+                return False
+        return True
