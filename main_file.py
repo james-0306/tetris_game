703807828d14931a5ad1e586b11719c135096c6e
james malitan <jamesmalitan21@gmail.com>
2026-06-20 11:08:02 +0800

    adding the display and speed of the game

--- main_file.py
+++ main_file.py
@@ -12,3 +12,9 @@ game_over_surface = title_font.render("Game Over", True, Colors.white)
 
 score_rect = pygame.Rect(320, 55, 170, 60)
 next_rect = pygame.Rect(320, 215, 170, 180)
+
+screen = pygame.display.set_mode((500, 620))
+pygame.display.set_caption("Tetris")
+
+clock = pygame.time.Clock()
+game = Game()
